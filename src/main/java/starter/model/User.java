fe22f02f542cab6de0aca0856dfe47f98b379f42
wwx <w17731138318@qq.com>
2018-01-20 12:31:39 +0800

    添加注释

--- src/main/java/starter/model/User.java
+++ src/main/java/starter/model/User.java
@@ -7,6 +7,14 @@ import java.util.List;
 
 /**
  * Created by Nasir on 12-09-2015.
+ * indexName：索引 ==mysql的数据库
+ * type：类型 ==mysql的表
+ * Documents：mysql的一行数据 一个json对象 每个document可包含多个field
+ * Fields：对应mysql的列
+ * shards：碎片
+ * replicas：备份
+ * Relational DB  ⇒ Databases ⇒ Tables  ⇒ Rows      ⇒ Columns
+ * Elasticsearch  ⇒ Indices   ⇒ Types   ⇒ Documents ⇒ Fields
  */
 @Document(indexName = "springboot", type = "user", shards = 5, replicas = 1)
 public class User {
